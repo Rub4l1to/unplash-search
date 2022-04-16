7768a640ffb40bab0d7ca67ac008df6c552ef809
Adrian <adrian.perez@gmail.com>
2022-04-16 22:12:46 +0200

    All aliases of possible routes are implemented.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,49 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-const path = require("path");
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
   resolve: {
     alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
       {
-        find: "@/components",
-        replacement: path.resolve(__dirname, "src/components"),
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
       },
-      { find: "@/styles", replacement: path.resolve(__dirname, "src/styles") },
-      { find: "@/hooks", replacement: path.resolve(__dirname, "src/hooks") },
       {
-        find: "@/screens",
-        replacement: path.resolve(__dirname, "src/screens"),
+        find: '@/assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
+      {
+        find: '@/components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@/context',
+        replacement: path.resolve(__dirname, 'src/context'),
+      },
+      {
+        find: '@/helpers',
+        replacement: path.resolve(__dirname, 'src/helpers'),
+      },
+      {
+        find: '@/hooks',
+        replacement: path.resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: '@/interfaces',
+        replacement: path.resolve(__dirname, 'src/interfaces'),
+      },
+      {
+        find: '@/styles',
+        replacement: path.resolve(__dirname, 'src/styles'),
+      },
+      {
+        find: '@/screens',
+        replacement: path.resolve(__dirname, 'src/screens'),
       },
     ],
   },
