7960fb1887e05d905fe556c57a49352ca2210d84
Adrian <adrian.perez@gmail.com>
2022-04-16 22:37:21 +0200

    Modified prettier configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,23 +2,10 @@ module.exports = {
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
-  printWidth: 200,
+  printWidth: 120,
   tabWidth: 2,
   endOfLine: 'auto',
   bracketSpacing: true,
   requirePragma: false,
   jsxSingleQuote: false,
 };
-
-// {
-//   "arrowParens": "always",
-//   "embeddedLanguageFormatting": "auto",
-//   "htmlWhitespaceSensitivity": "css",
-//   "insertPragma": false,
-//   "jsxBracketSameLine": false,
-//   "proseWrap": "preserve",
-//   "quoteProps": "as-needed",
-//   "trailingComma": "es5",
-//   "useTabs": false,
-//   "vueIndentScriptAndStyle": false,
-// }
