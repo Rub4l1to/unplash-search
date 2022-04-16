8987a1799576ff6a3f9bd3bb3dcbb78855b49e59
Adrian <adrian.perez@gmail.com>
2022-04-16 23:38:42 +0200

    New mixins `PaddingSize` & `MarginSize`

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,1 +1,23 @@
-import { css } from "styled-components";
+import { css } from 'styled-components';
+
+//* Interfaces
+import { IStyled } from '@/interfaces';
+
+export const PaddingSize = ({
+  startBlock = 0,
+  endBlock = 0,
+  startInline = 0,
+  endInline = 0,
+}: IStyled.SpaceProps) => css`
+  padding-inline-start: ${startInline}rem;
+  padding-inline-end: ${endInline}rem;
+  padding-block-start: ${startBlock}rem;
+  padding-block-end: ${endBlock}rem;
+`;
+
+export const MarginSize = ({ startBlock = 0, endBlock = 0, startInline = 0, endInline = 0 }: IStyled.SpaceProps) => css`
+  margin-inline-start: ${startInline}rem;
+  margin-inline-end: ${endInline}rem;
+  margin-block-start: ${startBlock}rem;
+  margin-block-end: ${endBlock}rem;
+`;
